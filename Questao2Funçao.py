4f549857147a25dcadc95353a3701efa61fae6b2
João Vitor Muniz Vasconcelos <140927075+JoaoVitorMuniz16@users.noreply.github.com>
2023-11-24 15:16:28 -0300

    Add files via upload

---
+++ Questao2Funçao.py
@@ -0,0 +1,19 @@
+def soma(n1,n2):
+    soma = n1 + n2
+    print (soma)
+def substração(n1,n2):
+    substração = n1 - n2
+    print (substração)
+def multiplicação(n1,n2):
+    multiplicação = n1 * n2
+    print(multiplicação)
+def divisão(n1,n2):
+    divisão = n1/n2
+    print(divisão)
+
+n1 = (int(input("Digite um Número:")))
+n2 = (int(input("Digite um outro Número:")))
+soma(n1,n2)
+substração(n1,n2)
+multiplicação(n1,n2)
+divisão(n1,n2)
